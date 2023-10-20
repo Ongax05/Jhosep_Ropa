4d8e497c6d52ef0fa3bdc4121a150142e1fc2812
Jhosep <jhoseppulido1129@gmail.com>
2023-10-20 17:50:09 -0500

    feat(Persistency): :sparkles: All dbsets added

--- Persistency/TemplateContext.cs
+++ Persistency/TemplateContext.cs
@@ -11,6 +11,29 @@ public class ApiDbContext : DbContext
         }
         public DbSet<Rol> Roles { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Cargo> Cargos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Color> Colores { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<DetalleOrden> DetallesOrdenes { get; set; }
+        public DbSet<DetalleVenta> DetallesVentas { get; set; }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Empresa> Empresa { get; set; }
+        public DbSet<Estado> Estados { get; set; }
+        public DbSet<FormaPago> FormasPagos { get; set; }
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Insumo> Insumos { get; set; }
+        public DbSet<Inventario> Inventarios { get; set; }
+        public DbSet<Municipio> Municipios { get; set; }
+        public DbSet<Orden> Ordenes { get; set; }
+        public DbSet<Pais> Paises { get; set; }
+        public DbSet<Prenda> Prendas { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<Talla> Tallas { get; set; }
+        public DbSet<TipoEstado> TiposEstados { get; set; }
+        public DbSet<TipoPersona> TiposPersonas { get; set; }
+        public DbSet<TipoProteccion> TiposProtecciones { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
