f724ec2dcb6c990c491c226023312d440f63f0ad
Jhosep <jhoseppulido1129@gmail.com>
2023-10-20 17:47:51 -0500

    feat(Domain): :sparkles: All interfaces linked to IUnitOfWork

--- Domain/Interfaces/IUnitOfWork.cs
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -4,5 +4,28 @@ public interface IUnitOfWork
 {
     IRolRepository Roles { get; }
     IUserRepository Users { get; }
+    ICargo Cargos { get; }
+    ICliente Clientes { get; }
+    IColor Colores { get; }
+    IDepartamento Departamentos { get; }
+    IDetalleOrden DetallesOrdenes { get; }
+    IDetalleVenta DetallesVentas { get; }
+    IEmpleado Empleados { get; }
+    IEmpresa Empresa { get; }
+    IEstado Estados { get; }
+    IFormaPago FormasPagos { get; }
+    IGenero Generos { get; }
+    IInsumo Insumos { get; }
+    IInventario Inventarios { get; }
+    IMunicipio Municipios { get; }
+    IOrden Ordenes { get; }
+    IPais Paises { get; }
+    IPrenda Prendas { get; }
+    IProveedor Proveedores { get; }
+    ITalla Tallas { get; }
+    ITipoEstado TiposEstados { get; }
+    ITipoPersona TiposPersonas { get; }
+    ITipoProteccion TiposProtecciones { get; }
+    IVenta Ventas { get; }
     Task<int> SaveAsync();
 }
